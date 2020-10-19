a48b0f6bb8f4edc8066b6d0c949a9f25144a4cf4
Waldemar Schroeer <lassmichinruhe@rz-amper.de>
2020-10-19 10:52:43 +0200

    Working.-)

--- test.sh
+++ test.sh
@@ -7,17 +7,16 @@
 # |                  waldemar.schroeer(at)rz-amper.de                       |
 # +-------------------------------------------------------------------------+
 
-config=test.json
-
-TestNumber=2
-echo ${TestNumber}
-echo "==============================="
-
-
-# ==== Einmal mittels EVAL =====================================================
+# ====  URLs ===================================================================
+# https://unix.stackexchange.com/questions/413878/json-array-to-bash-variables-using-jq
 # https://unix.stackexchange.com/questions/177843/parse-one-field-from-an-json-array-into-bash-array
 
-eval "$(jq -r '.[] | to_entries | .[] | .key + "=\"" + .value + "\""' < test.json)"
+
+config=test.json
+
+# ==== Einmal mittels EVAL -- ${key}=value =====================================
+# eval "$(jq -r '.[] | to_entries | .[] | .key + "=\"" + .value + "\""' < test.json)"
+eval "$(jq -r '.Teil1 | to_entries | .[] | .key + "=\"" + .value + "\""' < test.json)"
 
 echo "Name    = ${Name}"
 echo "Text    = ${Text}"
@@ -26,20 +25,13 @@ echo "Nummer2 = ${Nummer2}"
 echo "Nummer3 = ${Nummer3}"
 echo "==============================="
 
-
-# === In ein Array einlesen ====================================================
+# === In ein Array einlesen -- ${array[key]=value ==============================
 typeset -A myarraya
-
 while IFS== read -r key value; do
     myarraya[$key]=$value
-done < <(jq -r '.[] | to_entries | .[] | .key + "=" + .value ' test.json)
+done < <(jq -r '.Teil1 | to_entries | .[] | .key + "=" + .value ' test.json)
 
-# https://unix.stackexchange.com/questions/177843/parse-one-field-from-an-json-array-into-bash-array
-# IFS=$'\n' read -r key value -a myarraya[$key]=$value < <(set -o pipefail; jq -r '.[]' ${config} && printf '\0')
-
-# Array Definition auspumpen
 typeset -p myarraya
-
 echo "Name    = ${myarraya[Name]}"
 echo "Text    = ${myarraya[Text]}"
 echo "Nummer  = ${myarraya[Nummer1]}"
@@ -47,26 +39,15 @@ echo "Nummer  = ${myarraya[Nummer2]}"
 echo "Nummer  = ${myarraya[Nummer3]}"
 echo "==============================="
 
-
-
-# === In ein Array einlesen ====================================================
-# declare -A "myarrayb=($(jq -r '.[] | to_entries | .[] | .key + "=" + .value ' test.json))"
-declare -a "myarrayb=($(jq -r '.[] | to_entries | .[] | .value' test.json))"
+# === In ein Array einlesen [1..n] =============================================
+declare -a "myarrayb=($(jq -r '.Teil1 | to_entries | .[] | .value' test.json))"
+declare -a "myarrayc=($(jq -r '.Teil2 | to_entries | .[] | .value' test.json))"
 
 typeset -p myarrayb
 printf '%s\n' "${myarrayb[@]}"
 echo "==============================="
-
-
-
-
-
-
-
-
-
-
-
-
+typeset -p myarrayc
+printf '%s\n' "${myarrayc[@]}"
+echo "==============================="
 
 exit 0 
