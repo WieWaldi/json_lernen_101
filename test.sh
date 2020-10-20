32720de7cb577e5496c079c0cfdccf764d99a4d9
Waldemar Schroeer <lassmichinruhe@rz-amper.de>
2020-10-21 00:40:23 +0200

    Working.

--- test.sh
+++ test.sh
@@ -11,6 +11,9 @@
 # https://unix.stackexchange.com/questions/413878/json-array-to-bash-variables-using-jq
 # https://unix.stackexchange.com/questions/177843/parse-one-field-from-an-json-array-into-bash-array
 
+# bc kann nicht mit Flieskomma Exponenten umgehen. Doch kann es!
+#   https://unix.stackexchange.com/questions/258644/shell-scripting-calculate-power-of-a-number-with-a-real-number-as-an-exponent
+
 
 config=test.json
 
@@ -60,17 +63,44 @@ echo "myarrayb Nummer: $((myarrayb[5] + var_tmpa))"
 # === Noch mehr Rechnen ========================================================
 
 echo -e "\n\n==== Exponentieller Dreck ========================================="
-counter=50
+tempmax=110
+tempmin=40
 basis="2"
-output="ergebnis.txt"
-while [[ ${counter} -lt "115" ]]; do
-    funx=${counter}
-    funy=$(echo "${basis}^${funx}" | bc -l)
+steep=0.1
 
-    echo "x: ${funx}"
-    echo "y: ${funy}"
-    echo -e "${funx}\t${funy}" >> ${output}
-    let counter=counter+5
+# counter=50
+counter=${tempmin}
+output="ergebnis.txt"
+rm ${output}
+
+
+# Faktor berechnen
+xforfactor=$(echo "(${tempmax}-${tempmin})*${steep}" | bc -l)
+yforfactor=$(echo "e(l(${basis})*${xforfactor})-1" | bc -l)
+factor=$(echo "255/${yforfactor}" | bc -l)
+
+while [[ ${counter} -lt "115" ]]; do
+
+
+    # funx=$(( counter - tempmin ))
+    funx=$(echo "(${counter}-${tempmin})*${steep}" |bc -l)
+    # funy=$(echo "(${basis}^${funx})-1" | bc -l)
+    funytemp=$(echo "e(l(${basis})*${funx})-1" | bc -l)
+    # funy=$(echo "scale=0;${funytemp}/1" | bc)
+    funyfactor=$(echo "${funytemp}*${factor}" | bc)
+    funy=$(printf '%.*f\n' 0 ${funyfactor})
+
+
+    echo "counter:          ${counter}"
+    echo "funx:             ${funx}"
+    echo "funytemp:         ${funytemp}"
+    echo "funyfactor:       ${funyfactor}"
+    echo "funy:             ${funy}"
+    echo "yforfactor:       ${yforfactor}"
+    echo "factor:           ${factor}"
+    echo "==========================="
+    echo -e "${counter}\t${funy}" >> ${output}
+    let counter=counter+1
 done
 
 
